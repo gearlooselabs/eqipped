6ac65bbed5ab411452b796b2d1fe845ab35f8ba2
Harsh Kushwah <harshkingkushwah@gmail.com>
2022-07-22 13:58:45 +0530

    tailwind adding

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./resources/**/*.{html,js,css}","./resources/views/**/*.{ejs}","./resources/views/*.{ejs}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
